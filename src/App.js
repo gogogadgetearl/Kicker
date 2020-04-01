9e9200fe3f221d176d2412be3cac130ddb2baa68
Earl Mason <gogogadgetearl@gmail.com>
2020-04-01 18:15:47 -0400

    Metric=>Imperial; SVG sizing.

--- src/App.js
+++ src/App.js
@@ -28,9 +28,9 @@ class App extends Component {
         const radius = Math.round(height / (2 * (Math.sin(angleRad / 2) * Math.sin(angleRad / 2))) * 100) / 100;
         const length = (height / Math.sin(angleRad / 2)).toFixed(2);
         const foot = (height * Math.tan(angleBrad)).toFixed(2);
-        const deepth = (radius - radius * Math.sin((90 - angle / 2) * drc)).toFixed(2);
+        const depth = (radius - radius * Math.sin((90 - angle / 2) * drc)).toFixed(2);
 
-        const carr = {radius, length, foot, deepth};
+        const carr = {radius, length, foot, deepth: depth};
         return carr;
     }
     render() {
@@ -51,48 +51,45 @@ class App extends Component {
         const foot = this.cv().foot * size;
         const radius = this.cv().radius * size;
         return (
-            <div className="App">
+          <div className="App">
 
+            <div className='central'>
 
-
-
-              <div className='central'>
-
-                <div className="experiments"  style={printStyles}>
-                    <svg viewBox={"0 0 500 300"} height='17em' width="100%" preserveAspectRatio="xMidYMid meet">
-                        <rect width={foot} height={height} style={rectStyles} />
-                        <line id="newHeight" x1="0" y1="0" x2="0" y2={height} strokeWidth="8" stroke="blue"/>
-                        <line id="newFoot" x1="0" y1="0" x2={foot} y2="0" strokeWidth="8" stroke="purple"/>
-                        <path d={"M" + foot + "," + radius + " v-" + radius + " A" + radius + "," + radius + " 0 0,0 0," + height + " z"} fill="white" stroke="black" strokeWidth="3"/>
-                        <line id="newLength" x1="0" y1={height} x2={foot} y2="0" strokeWidth="2" stroke="green"/>
-                        <line id="redRadius" x1={foot} y1="0" x2={foot} y2={radius} stroke="orange" strokeWidth="3"/>
-                        <line id="redRadius" x1="0" y1={height} x2={foot} y2={radius} stroke="orange" strokeWidth="3"/>
-                        Sorry, your browser does not support inline SVG.
-                    </svg>
-                </div>
-
-              <div className="displayValues">
-
-                  <div style={radiusStyle}>radius<br/> {this.cv().radius}m</div>
-                  <div style={greentTextStyle}>length<br/> {this.cv().length}m</div>
-                  <div style={footStyle}>foot<br/> {this.cv().foot}m</div>
-                  <div style={deepthStyle}>deepth<br/> {this.cv().deepth}m</div>
-
+              <div className="experiments"  style={printStyles}>
+                <svg viewBox={"0 0 500 500"} height='500' width="100%" preserveAspectRatio="xMidYMid meet">
+                  <rect width={foot} height={height} style={rectStyles} />
+                  <line id="newHeight" x1="0" y1="0" x2="0" y2={height} strokeWidth="8" stroke="blue"/>
+                  <line id="newFoot" x1="0" y1="0" x2={foot} y2="0" strokeWidth="8" stroke="purple"/>
+                  <path d={"M" + foot + "," + radius + " v-" + radius + " A" + radius + "," + radius + " 0 0,0 0," + height + " z"} fill="white" stroke="black" strokeWidth="3"/>
+                  <line id="newLength" x1="0" y1={height} x2={foot} y2="0" strokeWidth="2" stroke="green"/>
+                  <line id="redRadius" x1={foot} y1="0" x2={foot} y2={radius} stroke="orange" strokeWidth="3"/>
+                  <line id="redRadius" x1="0" y1={height} x2={foot} y2={radius} stroke="orange" strokeWidth="3"/>
+                  Sorry, your browser does not support inline SVG.
+                </svg>
               </div>
 
+              <div className="displayValues">
+                <div style={radiusStyle}>radius<br/> {this.cv().radius}"</div>
+                <div style={greentTextStyle}>length<br/> {this.cv().length}"</div>
+                <div style={footStyle}>foot<br/> {this.cv().foot}"</div>
+                <div style={deepthStyle}>depth<br/> {this.cv().deepth}"</div>
+              </div>
 
               <div className="inputs">
-                <a className="numbers">ANGLE</a>
-                  <input name="angle" className="numbers" type="range" min="25" max="90" step="1" onChange={this.handleChange.bind(this)} value={this.state.angle}/>
-                <a className="numbers">{this.state.angle}&#176;</a>
-                  </div>
+                <span className="numbers">ANGLE</span>
+                <input name="angle" className="numbers" type="range" min="25" max="90" step="1" onChange={this.handleChange.bind(this)} value={this.state.angle}/>
+                <span className="numbers">{this.state.angle}&#176;</span>
+              </div>
 
-                  <div className="inputs" style={heightStyle}> <a className="numbers">   HEIGHT   </a>
-                        <input  name="height" type="range" min="0.1" max="4" step="0.1" onChange={this.handleChange.bind(this)} value={this.state.height}/>
-                      <a className="numbers">{this.state.height}m</a>
-                      </div>
-                </div>
+              <div className="inputs" style={heightStyle}>
+                <span className="numbers">HEIGHT</span>
+                <input  name="height" type="range" min="6" max="48" step="0.125" onChange={this.handleChange.bind(this)} value={this.state.height}/>
+                <span className="numbers">{this.state.height}"</span>
+              </div>
+
             </div>
+
+          </div>
         );
     }
 }
